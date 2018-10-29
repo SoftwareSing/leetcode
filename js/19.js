54a03b3918ab4bd3c5c7e68936ca55522e36a78b
SoftwareSing <SoftwareSing@gmail.com>
2018-10-30 02:15:38 +0800

    19. 首次寫出來的答案

---
+++ js/19.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  const recordList = [];
+  const recordNodeNumber = n + 1;
+  visitNode(head, recordNodeNumber, recordList);
+  if (isRemoveHead(recordList, recordNodeNumber)) {
+    // 表示應被刪的是head, 需要額外處理
+    head = head.next;
+  }
+  else {
+    changeLinkedNode(recordList, recordNodeNumber);
+  }
+
+  return head;
+};
+
+function visitNode(node, recordNodeNumber, recordList) {
+  recordList.push(node);
+  if (recordList.length > recordNodeNumber) {
+    recordList.shift();
+  }
+
+  if (node.next) {
+    visitNode(node.next, recordNodeNumber, recordList);
+  }
+}
+
+function isRemoveHead(recordList, recordNodeNumber) {
+  if (recordList.length < recordNodeNumber) {
+    return true;
+  }
+
+  return false;
+}
+
+function changeLinkedNode(recordList, recordNodeNumber) {
+  recordList[0].next = recordList[2];
+}
