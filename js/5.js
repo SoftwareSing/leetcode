d71074b88c740b3598ddefcc55ecc687934e5045
SoftwareSing <softwaresing@gmail.com>
2020-03-15 15:46:31 +0800

    Create 5.js
    Runtime: 448 ms, faster than 18.40% of JavaScript online submissions for Longest Palindromic Substring.
    Memory Usage: 36.1 MB, less than 67.39% of JavaScript online submissions for Longest Palindromic Substring.

---
+++ js/5.js
@@ -0,0 +1,62 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = function(s) {
+  if (s.length < 2) {
+    return s[0] || '';
+  }
+
+  const aIndexMap = new Map();
+  for (let i = 0; i < s.length; i += 1) {
+    const a = s[i];
+    const aIndexList = aIndexMap.get(a) || [];
+    aIndexList.push(i);
+    aIndexMap.set(a, aIndexList);
+  }
+
+  let answerStart = 0;
+  let answerEnd = 0;
+  for (const [a, aIndexList] of aIndexMap.entries()) {
+    if (aIndexList.length < 2) {
+      continue;
+    }
+
+    for (let i = 0; i < aIndexList.length; i += 1) {
+      for (let j = aIndexList.length - 1; j > i; j -= 1) {
+        const start = aIndexList[i];
+        const end = aIndexList[j];
+        if (start > answerStart && end < answerEnd) {
+          break;
+        }
+
+        if (isPalindromic(s, start, end)) {
+          if (end - start > answerEnd - answerStart) {
+            answerStart = start;
+            answerEnd = end;
+          }
+        }
+      }
+    }
+  }
+
+  return s.substring(answerStart, answerEnd + 1);
+};
+
+/**
+ * @param {String} s
+ * @param {Number} start
+ * @param {Number} end
+ * @return {Boolean}
+ */
+function isPalindromic(s, start, end) {
+  for (let i = 0; i < (end - start) / 2; i += 1) {
+    const headIndex = start + i;
+    const lastIndex = end - i;
+    if (s[headIndex] !== s[lastIndex]) {
+      return false;
+    }
+  }
+
+  return true;
+}
